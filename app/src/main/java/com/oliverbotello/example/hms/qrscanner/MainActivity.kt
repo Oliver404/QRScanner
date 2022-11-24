7a88b077bebf960a17f6ae38db50a83a597796e2
Oliver Botello <47994633+Oliver404@users.noreply.github.com>
2022-11-24 17:39:49 -0600

    Start commit

---
+++ app/src/main/java/com/oliverbotello/example/hms/qrscanner/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.oliverbotello.example.hms.qrscanner
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener, ActivityResultCallback<Boolean> {
+    private lateinit var btnScan: FloatingActionButton
+    private lateinit var txtVwResult: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+        txtVwResult = findViewById(R.id.txtvw_result)
+        btnScan = findViewById(R.id.fltbtn_scan_qr)
+
+        btnScan.setOnClickListener(this)
+    }
+
+    private fun validatePermission(): Boolean =
+        ContextCompat.checkSelfPermission(this.applicationContext, Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED
+
+    private fun scanQrCode() {
+        Snackbar.make(
+            btnScan,
+            "Read QR Code",
+            Snackbar.LENGTH_LONG
+        ).show()
+    }
+
+    private fun requestPermission() {
+        val requestPermissionLauncher = registerForActivityResult(RequestPermission(), this)
+
+        requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+    }
+
+    override fun onClick(v: View?) {
+        if (validatePermission()) scanQrCode()
+        else requestPermission()
+    }
+
+    override fun onActivityResult(result: Boolean) {
+        if (result)
+            scanQrCode()
+        else
+            Snackbar.make(
+                btnScan,
+                "Access to camera is necessary",
+                Snackbar.LENGTH_LONG
+            ).show()
+    }
+}
