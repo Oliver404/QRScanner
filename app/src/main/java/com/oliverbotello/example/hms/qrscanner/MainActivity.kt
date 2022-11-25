31c72b0cd442d700a3124f99bd5f5fc07150b768
Oliver Botello <47994633+Oliver404@users.noreply.github.com>
2022-11-24 18:05:02 -0600

    Read QR Code

--- app/src/main/java/com/oliverbotello/example/hms/qrscanner/MainActivity.kt
+++ app/src/main/java/com/oliverbotello/example/hms/qrscanner/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.oliverbotello.example.hms.qrscanner
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
@@ -11,17 +12,39 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import com.huawei.hms.hmsscankit.ScanUtil
+import com.huawei.hms.ml.scan.HmsScan
+import com.huawei.hms.ml.scan.HmsScanAnalyzerOptions
+import com.oliverbotello.example.hms.qrscanner.utils.REQUEST_SCAN_QR
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener, ActivityResultCallback<Boolean> {
+    // Components
     private lateinit var btnScan: FloatingActionButton
     private lateinit var txtVwResult: TextView
+    // Vars
     private val requestPermissionLauncher = registerForActivityResult(RequestPermission(), this)
+    private val options = HmsScanAnalyzerOptions.Creator().setHmsScanTypes(HmsScan.QRCODE_SCAN_TYPE)
+        .setViewType(1)
+        .create()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initView()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RESULT_OK && data != null) {
+            // if result is for QR Code
+            if (requestCode == REQUEST_SCAN_QR) {
+                val qrCodeResult: HmsScan? = data.getParcelableExtra(ScanUtil.RESULT)
+
+                setResult(qrCodeResult?.showResult)
+            }
+        }
     }
 
     private fun initView() {
@@ -31,16 +54,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, ActivityResultCa
         btnScan.setOnClickListener(this)
     }
 
+    private fun setResult(result: String?) {
+        txtVwResult.text = result?:"No Data"
+    }
+
     private fun validatePermission(): Boolean =
         ContextCompat.checkSelfPermission(this.applicationContext, Manifest.permission.CAMERA) ==
                 PackageManager.PERMISSION_GRANTED
 
     private fun scanQrCode() {
-        Snackbar.make(
-            btnScan,
-            "Read QR Code",
-            Snackbar.LENGTH_LONG
-        ).show()
+        ScanUtil.startScan(this, REQUEST_SCAN_QR, options)
     }
 
     private fun requestPermission() {
---
+++ app/src/main/java/com/oliverbotello/example/hms/qrscanner/utils/Const.kt
@@ -0,0 +1,3 @@
+package com.oliverbotello.example.hms.qrscanner.utils
+
+const val REQUEST_SCAN_QR = 200
